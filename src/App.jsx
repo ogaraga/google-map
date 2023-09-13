f24b2ff0346f31b5ab406aded35b745489bc8458
Ogaraga <raymond.ogaraga@gmail.com>
2023-09-13 10:29:20 +0100

    google-map

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>Google-map into react</h1>
+      <iframe  src="https://www.google.com/maps/embed/v1/place?q=Africa&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+      </div>
+        )
+}
+
+export default App
